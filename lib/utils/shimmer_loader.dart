290860a67cebf6905aba462af7e585da9b183298
Razu ahmed <99271821+razuahmed75@users.noreply.github.com>
2023-01-07 15:37:20 +0600

    added shadow in container

--- lib/utils/shimmer_loader.dart
+++ lib/utils/shimmer_loader.dart
@@ -23,17 +23,23 @@ class CardSkeleton extends StatelessWidget {
 
   Padding loader() {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8).r,
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20).r,
       child: Container(
         width: ScreenUtil().screenWidth,
         height: 80.h,
         decoration: BoxDecoration(
           color: white,
+          borderRadius: BorderRadius.circular(12.r),
           boxShadow: [
             BoxShadow(
               color: Colors.grey.shade300,
               blurRadius: 4.w,
-              offset: Offset(4, 8), // Shadow position
+              offset: Offset(0, 4), // Shadow position
+            ),
+            BoxShadow(
+              color: Colors.grey.shade300,
+              blurRadius: 4.w,
+              offset: Offset(0, -4), // Shadow position
             ),
           ],
         ),
